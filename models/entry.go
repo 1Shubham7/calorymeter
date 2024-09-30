d68a689930e1fc080ddd3b3009a01cff885f9aa5
Shubham Singh <shubhammahar1306@gmail.com>
2024-09-30 09:33:09 +0530

    added a readme
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

--- models/entry.go
+++ models/entry.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type FoodEntry struct {
-	ID          primitive.ObjectID `json:"id"`
+	ID          primitive.ObjectID `bson:"id"`
 	Dish        string             `json:"dish" binding:"required"`
 	Fat         string             `json:"fat"`
 	Protein     string             `json:"protein"`
