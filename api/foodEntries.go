1db24c92bc5e53d2708784c8a4f5fd2120de889e
Shubham Singh <shubhammahar1306@gmail.com>
2024-10-02 14:49:49 +0530

    added DeleteFoodEntry handler
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

--- api/foodEntries.go
+++ api/foodEntries.go
@@ -1,8 +1,18 @@
 package api
 
 import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+var entryCollection *mongo.Collection = openCollection(Client, "calories")
 
 func AddFoodEntry(ctx *gin.Context) {
 
@@ -29,5 +39,25 @@ func UpdateFoodIngredient(ctx *gin.Context) {
 }
 
 func DeleteFoodEntry(ctx *gin.Context) {
+	id := ctx.Params.ByName("id")
 
+	docID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	context, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+
+	result, err := entryCollection.DeleteOne(context, bson.M{"_id": docID})
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	defer cancel()
+	ctx.JSON(http.StatusOK, result.DeletedCount)
 }
