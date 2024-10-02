54d351749b33dad3464048b536df1e2b9b03e2d7
Shubham Singh <shubhammahar1306@gmail.com>
2024-10-02 14:16:37 +0530

    added blueprint for handlers
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

---
+++ api/foodEntries.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AddFoodEntry(ctx *gin.Context) {
+
+}
+
+func GetFoodEntries(ctx *gin.Context) {
+
+}
+
+func GetFoodEntry(ctx *gin.Context) {
+
+}
+
+func GetFoodEntryByIngredient(ctx *gin.Context) {
+
+}
+
+func UpdateFoodEntry(ctx *gin.Context) {
+
+}
+
+func UpdateFoodIngredient(ctx *gin.Context) {
+
+}
+
+func DeleteFoodEntry(ctx *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -19,7 +19,16 @@ func main() {
 	router.Use(gin.Logger())
 	router.Use(cors.Default())
 
-	router.POST("/food/create", api.CreateFoodEntry)
-	router.GET("/entries")
+	router.POST("/food/create", api.AddFoodEntry)
 
+	router.GET("/entries", api.GetFoodEntries)
+	router.GET("/entry/:id", api.GetFoodEntry)
+	router.GET("/ingredient/:ingredient", api.GetFoodEntryByIngredient)
+
+	router.PUT("/entry/update/:id", api.UpdateFoodEntry)
+	router.PUT("/ingredient/update/:id", api.UpdateFoodIngredient)
+
+	router.DELETE("/entry/delete/:id", api.DeleteFoodEntry)
+
+	router.Run(":" + port)
 }
