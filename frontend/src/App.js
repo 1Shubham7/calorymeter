ab118f237b9c14ed029497eacd02dab9650c6a42
Shubham Singh <shubhammahar1306@gmail.com>
2024-11-15 12:27:07 +0530

    added frontend sections
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,18 @@ import 'bootstrap/dist/css/bootstrap.css'
 
 import Entries from './components/entries.components'
 import TipDisplay from './components/AI/tip';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <div>
+      <Navbar />
+      <Hero />
       <Entries />
       <TipDisplay />
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Hero/Hero.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './hero.css'
+
+export default function Hero() {
+  return (
+    <>
+    <div className="caltech-segment">
+    <div className="hero-text">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure cum harum accusantium!
+      </div>
+      <div>
+        
+      </div>
+
+    </div>
+      
+    </>
+  )
+}
