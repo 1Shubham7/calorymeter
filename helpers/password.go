a47b54f19669b0550df4977e388c233862f32f66
Shubham Singh <shubhammahar1306@gmail.com>
2024-12-12 22:10:30 +0530

    added helper for handling hashpasswords
    Signed-off-by: Shubham Singh <shubhammahar1306@gmail.com>

---
+++ helpers/password.go
@@ -0,0 +1,22 @@
+package helpers
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if (err != nil) {
+		return "", err
+	}
+	return string(bytes), nil
+}
+
+func VerifyPassword(userPassword, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	// error means the password didn't match
+	if (err != nil) {
+		return false, "password didn't match"
+	}
+	return true, "password matches"
+}
